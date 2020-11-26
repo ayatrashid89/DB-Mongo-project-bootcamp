2b2dbb3a8ba7aacc9504b05d71e9002ba00e4394
Ayat Rashid <ayatomar2011@gmail.com>
2020-11-26 15:01:32 -0800

    create node-mongo

---
+++ node-express/routes/promotions.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const promotionsRouter = express.Router();
+
+promotionsRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the promotions to you');
+})
+.post((req, res) => {
+    res.end(`Will add the promotion: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+})
+.delete((req, res) => {
+    res.end('Deleting all promotions');
+});
+
+promotionsRouter.route('/:promotionsId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send details of the promotion: ${req.params.promotionsId} to you`);
+})
+.post((req, res) => {
+    res.end(`Will add the promotion:${req.params.promotionsId}  ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /promotions ${req.params.promotionsId}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting promotions ${req.params.promotionsId}`);
+});
+
+module.exports = promotionsRouter;
